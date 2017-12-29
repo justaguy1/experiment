d3ea97bc9e4dcb7fce3127d275a569b9cebc43b5
binaya poudel <lanthoo170@gmail.com>
2017-12-29 18:21:34 +0545

    freeze power

---
+++ My6thSemProject/src/mygame/SuperPowers.java
@@ -0,0 +1,15 @@
+package mygame;
+
+public class SuperPowers {
+	
+	 boolean freeze=false;
+	// use is moving boolean
+	
+	boolean speedup=false;
+	
+	boolean slowdown=false;
+	boolean normalspeed=false;
+	
+	 long powerTime=0;
+
+}
